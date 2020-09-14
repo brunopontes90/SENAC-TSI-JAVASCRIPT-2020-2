b52c3f7ef7d24e83146cdba2b188c83bd075e786
Bruno Pontes <53492661+brunopontes90@users.noreply.github.com>
2020-09-14 19:24:40 -0300

    aula 11-09-2020

--- aula 4/script.js
+++ aula 4/script.js
@@ -1,15 +1,8 @@
-let lakers = {
-    nome: 'Lakers',
-    pontos: [89, 120, 103, 100],
-    media: function() {
-        return this.pontos / 4;
-    }
-}
-
-let golden = {
-    nome: 'Golden State',
-    pontos: [97, 105, 110, 99],
-    media: function() {
+/*
+let georgia = {
+    nome: 'Georgia',
+    pontos: [99, 100, 101, 102, 104, 105],
+    media: function () {
         let soma = 0;
         for (let i = 0; i < this.pontos.length; i++) {
             soma += this.pontos[i];
@@ -18,10 +11,10 @@ let golden = {
     }
 }
 
-let cavaliers = {
-    nome: 'Cavaliers',
-    pontos: [90, 117, 108, 89],
-    media: function() {
+let newyork = {
+    nome: 'New York',
+    pontos: [105, 99, 75, 120, 119, 99],
+    media: function () {
         let soma = 0;
         for (let i = 0; i < this.pontos.length; i++) {
             soma += this.pontos[i];
@@ -30,57 +23,221 @@ let cavaliers = {
     }
 }
 
+let florida = {
+    nome: 'Florida',
+    pontos: [85, 80, 75, 125, 111, 98],
+    media: function () {
+        let soma = 0;
+        for (let i = 0; i < this.pontos.length; i++) {
+            soma += this.pontos[i];
+        }
+        return soma / this.pontos.length;
+    }
+}
 
-let nba = {
-    teams: [lakers, golden, cavaliers],
-    calculaMediaParaTodosOsTimes: function() {
-        for (let i = 0; i < this.teams.length; i++) { //cada cada time calcular a media
-            let timeAtual = this.teams[i]; //coloco o valor do time atual na variavel timeAtual
-            let soma = 0; //zera a variavel soma
-            for (let j = 0; j < timeAtual.pontos.length; j++) { //roda a soma para cada jogo
-                soma = timeAtual.pontos[j];
+let texas = {
+    nome: 'Texas',
+    pontos: [89, 122, 132, 85, 90, 99],
+    media: function () {
+        let soma = 0;
+        for (let i = 0; i < this.pontos.length; i++) {
+            soma += this.pontos[i];
+        }
+        return soma / this.pontos.length;
+    }
+}
+
+let ncaa = {
+    times: [georgia, newyork, florida, texas],
+    vencedor: function () {
+        let campeao = this.times[0]; //armazenando o objeto (georgia)
+        //console.log("A média do time " + this.times[0].nome + " é:" + this.times[0].media());
+        for (let i = 1; i < this.times.length; i++) {
+            //console.log("A média do time " + this.times[i].nome + " é:" + this.times[i].media());
+            if (this.times[i].media() > campeao.media()) {
+                campeao = this.times[i]; //armazenado o objeto (this.time[i])
             }
-            timeAtual.media = soma / timeAtual.pontos.length; //cria um novo valor para cada time
         }
+        console.log(campeao.nome + ' é o vencedor');
+    }
+}
+
+ncaa.vencedor();
+
+function rodaOdado() {
+    let dado = Math.floor(Math.random() * 6) + 1;
+    document.querySelector('#dado').textContent = dado;
+}
+
+*/
+
+let dado = {
+    valor: null,
+    jogando: function() {
+        /**
+         * Gera um número aleatório de 1 até 6 e atualiza na tela esse novo valor
+         */
+        this.valor = Math.floor(Math.random() * 6) + 1; //gera um numero randomico
+        this.imprimeValor();
     },
-    quemEhOVencedor: function() {
-        this.calculaMediaParaTodosOsTimes();
-        let vencedor = this.teams[0]; //atribui a 1º posição dos times como vencedor
-        for (let i = 0; i < this.teams.length; i++) { //roda cada  um dos times
-            if (vencedor.media < this.teams[i].media) { //verifico se o numero de pontos é maior do que o vencedor atual
-                vencedor = this.teams[i]; //guardo o vencedor novo
-            }
-        }
-        console.log("O grande vencedor é o " + vencedor.nome); //imprimo o nome do  vencedor
+    imprimeValor: function() {
+        /**
+         * Atualiza na tela o valor do dado atual no elemento HMTL (#dado)
+         */
+        document.querySelector('#dado').textContent = this.valor; //substitui o valor do texto do elemento HTML com o ID=dado
+    }
+};
+
+let player1 = {
+    nome: 'Player 1',
+    id: 1,
+    pontos: 0,
+    pontosTemporarios: 0,
+    jogaODado: function() {
+        /**
+         * O jogador deve rolar os dados e pedir para o jogo verificar
+         */
+        dado.jogando();
+        pig.verificaOValorDoDado();
+    },
+    parar: function() {
+        /**
+         * Caso o jogador decida parar, atribuir os valores dos pontos temporarios nos pontos oficiais
+         * e passar a vez para o próximo jogador
+         */
+        this.adicionarNosPontos();
+        pig.proximoJogador();
+    },
+    adicionarNosPontosTemporarios: function(pontos) {
+        /**
+         * Calcula baseado nos pontos temporarios anteriores a soma dos novos valores do dado
+         * Escreve esse novo valor na tela
+         */
+        this.pontosTemporarios += pontos;
+        this.imprimirOsPontosTemporarios();
+    },
+    zerarOsPontosTemporarios: function() {
+        /**
+         * Zera os pontos temporarios do jogador
+         */
+        this.pontosTemporarios = 0;
+        this.imprimirOsPontosTemporarios();
+    },
+    adicionarNosPontos: function() {
+        /**
+         * Adiciona aos pontos oficiais o valor dos pontos temporários
+         */
+        this.pontos += this.pontosTemporarios;
+        this.imprimirOsPontos();
+    },
+    imprimirOsPontosTemporarios: function() {
+        /**
+         * Atualiza na tela o elemento com o ID (#jogador-1-pontosTemporarios) o valor dos novos pontos temporário
+         */
+        document.querySelector('#jogador-' + this.id + '-pontosTemporarios').textContent = this.pontosTemporarios;
+    },
+    imprimirOsPontos: function() {
+        /**
+         * Atualiza na tela o elemento com o ID (#jogador-1-pontos) o valor dos novos pontos
+         */
+        document.querySelector('#jogador-' + this.id + '-pontos').textContent = this.pontos;
+    }
+}
+
+let player2 = {
+    nome: 'Player 2',
+    id: 2,
+    pontos: 0,
+    pontosTemporarios: 0,
+    jogaODado: function() {
+        /**
+         * O jogador deve rolar os dados e pedir para o jogo verificar
+         */
+        dado.jogando();
+        pig.verificaOValorDoDado();
+    },
+    parar: function() {
+        /**
+         * Caso o jogador decida parar, atribuir os valores dos pontos temporarios nos pontos oficiais
+         * e passar a vez para o próximo jogador
+         */
+        this.adicionarNosPontos();
+        pig.proximoJogador();
+    },
+    adicionarNosPontosTemporarios: function(pontos) {
+        /**
+         * Calcula baseado nos pontos temporarios anteriores a soma dos novos valores do dado
+         * Escreve esse novo valor na tela
+         */
+        this.pontosTemporarios += pontos;
+        this.imprimirOsPontosTemporarios();
+    },
+    zerarOsPontosTemporarios: function() {
+        /**
+         * Zera os pontos temporarios do jogador
+         */
+        this.pontosTemporarios = 0;
+        this.imprimirOsPontosTemporarios();
+    },
+    adicionarNosPontos: function() {
+        /**
+         * Adiciona aos pontos oficiais o valor dos pontos temporários
+         */
+        this.pontos += this.pontosTemporarios;
+        this.imprimirOsPontos();
+    },
+    imprimirOsPontosTemporarios: function() {
+        /**
+         * Atualiza na tela o elemento com o ID (#jogador-1-pontosTemporarios) o valor dos novos pontos temporário
+         */
+        document.querySelector('#jogador-' + this.id + '-pontosTemporarios').textContent = this.pontosTemporarios;
+    },
+    imprimirOsPontos: function() {
+        /**
+         * Atualiza na tela o elemento com o ID (#jogador-1-pontos) o valor dos novos pontos
+         */
+        document.querySelector('#jogador-' + this.id + '-pontos').textContent = this.pontos;
     }
 }
 
 
-console.log(nba.teams);
-nba.quemEhOVencedor();
-// console.log(lakers.nome, lakers.media());
-// console.log(golden.nome, golden.media());
-// console.log(cavaliers.nome, cavaliers.media());
-
-class timeDeBasquete {
-
-    constructor(_name, _pontos) {
-        this.name = _name;
-        this.pontos = _pontos;
-    }
-
-    media() {
-        let soma = 0;
-        for (let i = 0; i < this.pontos.length; i++) {
-            soma += this.pontos[i];
+let pig = {
+    jogadores: [player1, player2],
+    jogadorAtual: 0,
+    playerAtual: function() {
+        /** 
+         * Retorna em objeto o jogador da vez
+         */
+        return this.jogadores[this.jogadorAtual];
+    },
+    proximoJogador: function() {
+        /**
+         * Quando for passar a vez, é necessário zerar o valor de pontos temporarios do jogador atual.
+         * Passar a vez para o próximo jogador, e atualizar na tela o nome dele
+         */
+        this.playerAtual().zerarOsPontosTemporarios();
+        this.jogadorAtual++; //Passa para o próximo jogador
+        if (this.jogadorAtual == this.jogadores.length) { //se o próximo jogador não estiver no array
+            this.jogadorAtual = 0; //volto para primeiro jogador 
         }
-        return soma / this.pontos.length;
+        document.querySelector('#jogador-atual').textContent = this.playerAtual().nome; //atualiza o nome do jogador no elemetno HTML (#jogador-atual)
+    },
+    verificaOValorDoDado: function() {
+        /**
+         * Rega do jogo: 
+         * Caso o jogador tire 1: passar a vez para o próximo jogador
+         * Caso o jogador tire ourto número: adicionar aos pontos temporários o valor atual do dado
+         */
+        if (dado.valor == 1) {
+            this.proximoJogador();
+        } else {
+            this.playerAtual().adicionarNosPontosTemporarios(dado.valor);
+        }
     }
 }
 
-times = [
-    lakers = new timeDeBasquete("Lakers", [89, 120, 103, 100]),
-    golden = new timeDeBasquete("Golden", [97, 105, 110, 99])
-]
+window.onload = alerta;
 
-console.log(times);
+function alerta() {
+    alert(1);
+}
